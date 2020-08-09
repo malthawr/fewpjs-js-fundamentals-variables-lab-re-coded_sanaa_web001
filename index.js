126616f232af02113cdd76dca36b81a5235e69e8
Mohammed Al-Thawr <malthawr@gmail.com>
2020-08-09 17:30:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 
 
 let companyCeo = 'Susan Smith';
+=======
+let companyName = 'Scuber';
+const companyName = 'Scuber';
+let mostProfitableNeighborhoo = 'Chelsea';
+mostProfitableNeighborhood = 'Upper West Side';
+
+let companyCeo = 'Susan Smith';
+companyCeo = 'Lauren Hart';
+>>>>>>> f945fbdcd396714077d1a892f9a317803a0b6666
